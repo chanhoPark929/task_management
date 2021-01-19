5a638d3de614f1dc28e168db87e3985eae8b8b06
Chanho Park <chanho929@outlook.com>
2021-01-20 04:24:13 +0800

    creating a task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './task.model';
+import { Task, Taskstatus } from './task.model';
+import { v1 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -7,5 +9,19 @@ export class TasksService {
 	
 	getAllTasks(): Task[] {
 		return this.tasks;
+	}
+
+	createTask(createTaskDto: CreateTaskDto): Task {
+		const { title, description } = createTaskDto;
+		
+		const task: Task = {
+			id: uuid(),
+			title,
+			description,
+			status: Taskstatus.OPEN,
+		};
+
+		this.tasks.push(task);
+		return task;
 	}
 }
